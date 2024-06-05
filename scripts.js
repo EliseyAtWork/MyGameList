bfe7ab9e5df6b196c410e1e0df492c17303cfd56
EliseyAtWork <giperqub@gmail.com>
2024-06-06 00:43:16 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-var meguminRef = document.getElementById("MeguminImg");
-
-meguminRef.onclick = function() {
-    var currentImg = meguminRef.getAttribute("src");
-    if (currentImg == "content/Довольная Мегумин на шапку.png")
-        meguminRef.setAttribute("src", "content/Мегумин на шапку.png");
-    else
-        meguminRef.setAttribute("src", "content/Довольная Мегумин на шапку.png");
-}
